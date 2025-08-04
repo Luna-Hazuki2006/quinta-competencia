b8b97b1f22346aef60a35f9ea8a4b90cf776716a
Luna Hazuki <lunahazuki2006@outlook.com>
2025-08-04 12:17:06 -0400

    Aprueba de balas

--- tercero.py
+++ tercero.py
@@ -16,18 +16,25 @@ def bucleado(numero : int):
     return resultado
 
 def bucle(numero : int): 
-    print('------------------------------------------------------------')
-    inicio = datetime.now()
-    dato = numero
-    resultado = bucleado(dato)
-    final = datetime.now()
-    print(f'Inicio del bucle: {inicio} \nFin del bucle: {final} \nDiferencia de tiempo: {final - inicio}')
-    if resultado >= 1000000000: 
-        visible = str(resultado)
-        print(f'Resultado del factorial {numero}: {visible[0:3]}...{visible[-4:-1]}')
-    else: print(f'Resultado del factorial {numero}: {resultado}')
-    print('------------------------------------------------------------')
-    return resultado
+    if numero <= 0: 
+        print('El número tiene que ser mayor a 0')
+        return
+    try: 
+        numero = int(numero)
+        print('------------------------------------------------------------')
+        inicio = datetime.now()
+        dato = numero
+        resultado = bucleado(dato)
+        final = datetime.now()
+        print(f'Inicio del bucle: {inicio} \nFin del bucle: {final} \nDiferencia de tiempo: {final - inicio}')
+        if resultado >= 1000000000: 
+            visible = str(resultado)
+            print(f'Resultado del factorial {numero}: {visible[0:3]}...{visible[-4:-1]}')
+        else: print(f'Resultado del factorial {numero}: {resultado}')
+        print('------------------------------------------------------------')
+        return resultado
+    except: 
+        print('El valor está en un formato no entero')
 
 def recurseado(numero : int, resultado : int = 1):
     try: 
@@ -39,24 +46,31 @@ def recurseado(numero : int, resultado : int = 1):
         return numero, resultado
 
 def recursor(numero : int): 
-    print('------------------------------------------------------------')
-    inicio = datetime.now()
-    dato = numero
-    resultado = 1
-    while dato != 1: 
-        dato, resultado = recurseado(dato, resultado)
-    final = datetime.now()
-    print(f'Inicio de la recursividad: {inicio} \nFin de la recursividad: {final} \nDiferencia de tiempo: {final - inicio}')
-    if resultado >= 1000000000: 
-        visible = str(resultado)
-        print(f'Resultado del factorial {numero}: {visible[0:3]}...{visible[-4:-1]}')
-    else: print(f'Resultado del factorial {numero}: {resultado}')
-    print('------------------------------------------------------------')
-    return resultado
+    if numero <= 0: 
+        print('El número tiene que ser mayor a 0')
+        return
+    try: 
+        numero = int(numero)
+        print('------------------------------------------------------------')
+        inicio = datetime.now()
+        dato = numero
+        resultado = 1
+        while dato != 1: 
+            dato, resultado = recurseado(dato, resultado)
+        final = datetime.now()
+        print(f'Inicio de la recursividad: {inicio} \nFin de la recursividad: {final} \nDiferencia de tiempo: {final - inicio}')
+        if resultado >= 1000000000: 
+            visible = str(resultado)
+            print(f'Resultado del factorial {numero}: {visible[0:3]}...{visible[-4:-1]}')
+        else: print(f'Resultado del factorial {numero}: {resultado}')
+        print('------------------------------------------------------------')
+        return resultado
+    except: 
+        print('El valor está en un formato no entero')
 
 def main(): 
-    bucle(5)
-    recursor(6)
+    bucle(0)
+    recursor(0)
 
 if __name__ == '__main__': 
     main()
