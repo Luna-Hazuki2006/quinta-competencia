2999e8e40c4f18b9fc701cac2fb45c952ae15cc4
Luna Hazuki <lunahazuki2006@outlook.com>
2025-08-06 12:18:17 -0400

    Completamenre funcional

--- septimo.py
+++ septimo.py
@@ -5,16 +5,18 @@ def llegar(matriz : list[list[int]]):
     tabu = []
     coordenadas = []
     coordenadas.append((0, 0))
+    final_y = len(matriz) - 1
+    final_x = len(matriz[0]) - 1
     # tabu.append((inicio[1], inicio[0]))
     for _ in range(len(matriz) * len(matriz[0])): 
         x = inicio[0]
         y = inicio[1]
         for cada in direcciones: 
             # Solo uno saldrá -------------
-            if cada == '↓' and y < 4 and matriz[y + 1][x] != 1 and ((y + 1, x), cada) not in tabu: 
+            if cada == '↓' and y < final_y and matriz[y + 1][x] != 1 and ((y + 1, x), cada) not in tabu: 
                 y += 1
                 x = inicio[0]
-            elif cada == '→' and x < 4 and matriz[y][x + 1] != 1 and ((y, x + 1), cada) not in tabu:
+            elif cada == '→' and x < final_x and matriz[y][x + 1] != 1 and ((y, x + 1), cada) not in tabu:
                 x += 1
                 y = inicio[1]
             elif cada == '↑' and y > 0 and matriz[y - 1][x] != 1 and ((y - 1, x), cada) not in tabu:
@@ -62,7 +64,11 @@ def llegar(matriz : list[list[int]]):
             #     tabu.append((y, x))
         if inicio[0] == 4 and inicio[1] == 4: 
             break
+    if coordenadas[-1] != (final_y, final_x): 
+        print('No se pudo conseguir un camino al final')
+        return
     print(tabu)
+    print(coordenadas)
     print(camino)
 
 def main(): 
